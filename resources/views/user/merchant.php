1009506be9e3bf5129e60db97a20bd3dd809bc08
DESKTOP-99VFG60\Shafa Tathya <shafatathya@gmail.com>
2021-03-24 14:05:53 +0700

    merchant page ref #noissue

--- resources/views/user/merchant.php
+++ resources/views/user/merchant.php
@@ -4,4 +4,18 @@
 <div class="container">
   <!-- Content here -->
 </div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>
 @endsection
