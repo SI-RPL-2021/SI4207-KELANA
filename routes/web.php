25400120b0219e3ffcf0b461725ca3507336ea37
rizkyfadilah22 <rizkyfadilah22@gmail.com>
2021-04-01 10:19:49 +0700

    penyamaan dengan branch master

--- routes/web.php
+++ routes/web.php
@@ -24,14 +24,6 @@ Auth::routes();
 
 Route::get('/user', [App\Http\Controllers\HomeController::class, 'index'])->name('user');
 Route::get('/admin', [App\Http\Controllers\HomeController::class, 'handleAdmin'])->name('admin.route')->middleware('admin');
-<<<<<<< HEAD
-Route::get('/explore', [App\Http\Controllers\HomeController::class, 'explore'])->name(''); //route ke index explore
-Route::get('/explore/tempatwisata', [App\Http\Controllers\HomeController::class, 'tempatwisata'])->name(''); //route ke tempat wisata
-Route::get('/story', [App\Http\Controllers\StoryController::class, 'index'])->name(''); //route ke index story
-
-=======
-
 
 Route::resource('detailuser', DetailuserController::class);
 Route::resource('story', StoryController::class);
->>>>>>> 71135fcd8e38f57b7ed625bd9038feaae9456172
