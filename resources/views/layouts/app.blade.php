5504af3ccfedeb28d8cf40c615c8c43c742520c2
ihsbramn <abdurahmanihsan25@gmail.com>
2021-04-14 13:19:49 +0700

    add method store merchant controller ref #33

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -102,7 +102,7 @@
                                         @endif
                                     @endauth
                                     <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                                document.getElementById('logout-form').submit();">
+                                                                    document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
---
+++ resources/views/merchant/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <br><br><br><br>
+    <br><br><br><br>
+    <div class="container">
+        <p>create merchant page </p>
+    </div>
+@endsection
