1d7942626730a4eac545e76787132df43067836e
rizkyfadilah22 <rizkyfadilah22@gmail.com>
2021-04-27 22:28:57 +0700

    pembuatan migrasi ke database pada page tempat wisata, kuliner, dan penginapan ref #24 #26 #27

--- resources/views/hotel/index.blade.php
+++ resources/views/hotel/index.blade.php
@@ -3,10 +3,45 @@
 @section('content')
 
 <br><br><br>    
-
-<h1 style="text-align: center">Penginapan</h1>
+<h1 class="text-center" style="color: #69D1C5; -webkit-text-stroke-width: 0.5px; -webkit-text-stroke-color: black;">Penginapan</h1>
 <h5 style="text-align: center">Looking for a place to stay? Here you go!</h5> 
-<br><br><br><br>
+<br><br>
+
+<div style="margin-left: 20px; margin-right: 20px;">
+
+    <div class="input-group mb-3" style="width: 300px">
+        <input type="text" class="form-control" placeholder="Filter" aria-label="Filter" aria-describedby="button-addon2">
+    </div>
     
+    <section>
+        <?php
+        //Columns must be a factor of 12 (1,2,3,4,6,12)
+        $numOfCols = 6;
+        $rowCount = 0;
+        $bootstrapColWidth = 12 / $numOfCols;
+        foreach ($hotel as $htl) {
+
+        if ($rowCount % $numOfCols == 0) { ?> <div class="row"> <?php }
+            $rowCount++;
+            ?>
+            <div class="col-md-<?php echo $bootstrapColWidth; ?>">
+                <div class="card">
+                    <img src="/images/{{ $htl->hotel_img }}" class="card-img-top img-fluid" alt="hotel_img">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $htl->hotel_title }}</h5>
+                        <a href="{{ route('hotel.show', $htl->id) }}" class="btn btn-primary">Read more</a>
+                    </div>
+                </div>
+                <br>
+            </div>
+            <?php if ($rowCount % $numOfCols == 0) { ?>
+        </div> <?php }
+        }
+        ?>
+
+    </section>
+
+</div>
+
 
 @endsection
